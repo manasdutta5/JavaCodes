e91c3805b06ca9012893da0e1d0aabf36ff709f8
Manas Dutta <manas.dutta197@gmail.com>
2022-05-20 15:34:12 +0530

    DSA

---
+++ src/Algorithm/Kadanes_Algo.java
@@ -0,0 +1,27 @@
+package Algorithm;
+import java.util.*;
+
+public class Kadanes_Algo {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,-2,5};
+        System.out.println("Maximum conntiguous subarray sum is: "+ maxSubarraySum(arr,5));
+    }
+
+    static long maxSubarraySum(int arr[], int n){
+
+        // Your code here
+        int current_sum=0,max_sum=Integer. MIN_VALUE;
+
+        for(int i=0;i<n;i++)
+        {
+            if(current_sum < 0)
+            {
+                current_sum = 0;
+            }
+
+            current_sum += arr[i];
+            max_sum = Math.max(current_sum,max_sum);
+        }
+        return max_sum;
+    }
+}
---
+++ src/DataStructures/Graphs/Bfs.java
@@ -0,0 +1,10 @@
+package DataStructures.Graphs;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Bfs {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+    }
+}
